f88a6eebe632e951bc81f60bf20de6bfa9499514
iochoa808 <isaacochoagarriga@gmail.com>
2024-12-14 18:46:11 +0100

    Update Spotify.py

--- src/Spotify.py
+++ src/Spotify.py
@@ -1,7 +1,7 @@
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
 print("BEFORE LOADENV")
@@ -9,14 +9,15 @@ print("BEFORE LOADENV")
 load_dotenv()
 
 sp_oauth = SpotifyOAuth(
-    client_id=os.getenv("CLIENT_ID"),
-    client_secret=os.getenv("CLIENT_SECRET"),
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"],
     redirect_uri="http://localhost:8888/callback",
     scope="user-read-recently-played",
-    cache_path="/tmp/spotify_cache"
+    cache_path="token.txt"
 )
 
-access_token = sp_oauth.refresh_access_token(refresh_token=os.getenv('REFRESH_TOKEN'))["access_token"]
+token_info = sp_oauth.refresh_access_token(os.environ["REFRESH_TOKEN"])
+access_token = token_info["access_token"]
 
 sp = spotipy.Spotify(auth=access_token)
 
