cdbba196d9f8f29c2fa6e5066d674c1520c91d80
iochoa808 <isaacochoagarriga@gmail.com>
2024-12-14 16:33:49 +0100

    Print logs per debuggar

--- src/Spotify.py
+++ src/Spotify.py
@@ -4,7 +4,12 @@ import spotipy
 from dotenv import load_dotenv
 import os
 
+print("BEFORE LOADENV")
+
 load_dotenv()
+
+print(f"CLIENT_ID: {os.getenv('CLIENT_ID')}")
+print(f"CLIENT_SECRET: {os.getenv('CLIENT_SECRET')}")
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=os.getenv("CLIENT_ID"),
@@ -12,3 +17,5 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     redirect_uri="http://localhost:8888/callback",
     scope="user-read-recently-played"
 ))
+
+print("SPOTIPY INITIATED")
